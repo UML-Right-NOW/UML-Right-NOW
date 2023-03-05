20ba01c0b0a142e3de759828df90b5f65d5cdcf8
Sam Claflin <sam.claflin@edgesoftware.dev>
2023-03-05 15:47:32 -0500

    Installed and configured Tailwind; formatted 'hello.ts'; added element in 'index.tsx' to test Tailwind installation

--- uml-right-now/src/pages/index.tsx
+++ uml-right-now/src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
+                <h1 className="font-bold underline">Hello, World!</h1>
             </main>
         </>
     );
