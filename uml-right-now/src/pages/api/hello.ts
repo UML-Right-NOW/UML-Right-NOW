cadb404a6eec80d29e7495ad91948670d145cec1
YOUTE01806251 <johneyoute@gMAIL.com>
2023-03-22 12:49:35 -0400

    commit try

--- uml-right-now/src/pages/api/hello.ts
+++ uml-right-now/src/pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
 import clientPromise from "@/mongo";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
     name: string
