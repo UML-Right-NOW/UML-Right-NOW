639a54e6781d9e44223d72a6d9ad6debbff7a95a
YOUTE01806251 <johneyoute@gMAIL.com>
2023-03-26 19:56:18 -0400

    Update of the profile page

--- uml-right-now/src/pages/ProfilePage.jsx
+++ uml-right-now/src/pages/ProfilePage.jsx
@@ -1,17 +1,11 @@
-
-
 import SideNav from "../components/SideNav/SideNav";
-
-
 
 export default function ProfilePage(props) {
     const url = "https://i.pravatar.cc/150?u=a042581f4e29026024d"
+
     return (
-        <div >
-            <SideNav className="h-80 w-80" />
-
-
-
+        <div>
+            <SideNav />
         </div>
     )
 }
