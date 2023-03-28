884c6a5ae39d9604b2df7af933e5a1b04bd0b01a
YOUTE01806251 <johneyoute@gMAIL.com>
2023-03-28 16:41:09 -0400

    The generate button can now redirect.... it is only for the demo...

--- uml-right-now/src/pages/index.tsx
+++ uml-right-now/src/pages/index.tsx
@@ -1,16 +1,17 @@
 import TranscriptInput from "@/components/TranscriptInput/TranscriptInput";
 import { Tooltip } from "@nextui-org/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import SearchBar from "../components/SearchBar/SearchBar";
 
 export default function Home() {
     const backgroundimage1 = "https://images.pexels.com/photos/6147369/pexels-photo-6147369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
     const [entry, setEntry] = useState("");
+    const router = useRouter();
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    function handleInput() {
-
+    function handleClick(event: any) {
+        router.push("/pathways");
     }
 
     return (
@@ -25,11 +26,16 @@ export default function Home() {
                 <div className="bg-rowdy-blue w-full backdrop-brightness-50 h-screen flex bg-rowdy-blue bg-cover bg-no-repeat bg-center" style={{ backgroundImage: `url(${backgroundimage1})` }} >
                     <div className="absolute w-full h-full">
                         <h1 className="md:text-4xl lg:text-9xl xl:text-7xl font-sans mt-20 text-center text-white mb-9">Enter Your <em className="xl:text-9xl text-rowdy-blue underline">major</em>  to get started</h1>
-                        <div className=" flex justify-center mt-px" >
-                            <SearchBar />
+                        <div className=" flex flex-col justify-center  mt-px">
+                            <div className=" flex justify-center mt-px">
+                                <SearchBar />
+                            </div>
+                            <div className=" flex justify-center mt-px">
+                                <button className="bg-rowdy-blue w-32 h-10 border-white border-4 text-white mt-5 rounded-3xl " onClick={handleClick} >Generate</button>
+                            </div>
                         </div>
-                        <div className=" flex justify-center" >
-                            <h1 className="text-9xl text-white">-or-</h1>
+                        <div className=" flex  justify-center" >
+                            <h1 className="text-7xl mt-6 text-white">-or-</h1>
                         </div>
                         <div className=" flex justify-center" >
                             <Tooltip content={"Upload Your UML transcript so we can help you better with your degree path"} color="primary" placement="rightEnd">
