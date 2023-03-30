89884f134674a2ecd3c4e73a099fbebb331705c4
Sam Claflin <sam.claflin@edgesoftware.dev>
2023-03-30 16:35:14 -0400

    Added comprehensive list of majors to search bar

--- uml-right-now/src/components/SearchBar/SearchBar.jsx
+++ uml-right-now/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import { majors } from "@/Majors";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -37,34 +38,9 @@ export default function SearchBar() {
             disablePortal
             id="combo-box-demo"
             classes={classes}
-            options={DegreePaths}
+            options={majors}
             sx={{ width: 400, WebkitTextFillColor: "white", }}
             renderInput={(params) => <TextField {...params} label="Degree pathways" className='stroke-white stroke-7' />}
         />
     );
 }
-
-// classes exemples
-const DegreePaths = [
-    { label: "Computer science general", year: 2023 },
-    { label: "Computer science software", year: 2023 },
-    { label: "Biology", year: 2023 },
-    { label: "Lawyer", year: 2023 },
-    { label: "Math", year: 2023 },
-    { label: "MIS", year: 2023 },
-    { label: "Software degree", year: 2023 },
-    {
-        label: "Buisseness",
-        year: 2022,
-    },
-    { label: "Accountant", year: 2023 },
-    { label: "English", year: 2023 },
-    {
-        label: "French",
-        year: 2003,
-    },
-    {
-        label: "Geography",
-        year: 2023,
-    }
-];
